24317ee9f1cb88a586b8e96a13b283c58b73ef7b
azufb <curiousazunas@gmail.com>
2022-04-17 17:25:33 +0900

    ヘッダにCSS適用

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,8 @@
+import { headerStyle } from "../styles/header.css"
+
 export const Header = () => {
     return (
-        <div>
+        <div className={headerStyle}>
             <h1>タスク管理</h1>
         </div>
     )
