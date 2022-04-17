924f67aa346c7e015a4bf655ee9921361257de60
azufb <curiousazunas@gmail.com>
2022-04-17 15:54:55 +0900

    新しいコンポーネントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import ToDos from './components/todos';
 import DoneToDos from "./components/doneTodos";
 import Achievement from "./components/achievement";
 import DeleteAllBtn from "./components/deleteAllBtn";
+import { Header } from "./components/header";
+import { Footer } from "./components/footer";
 
 export const App = () => {
     return (
         <>
+            <Header />
             <Form />
             <DeleteAllBtn />
             <ToDos />
             <DoneToDos />
             <Achievement />
+            <Footer />
         </>
     )
 }
