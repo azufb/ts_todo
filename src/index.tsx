6b48a445b93a48540f1daea12b8dd4a8a904e958
azufb <curiousazunas@gmail.com>
2022-04-17 15:55:28 +0900

    全体で共通のスタイルを適用

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { store } from './store/store';
 import { Provider } from 'react-redux';
+import "./styles/global.css";
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
