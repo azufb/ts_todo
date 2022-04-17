26d723a9b5a3d1a09bfad05167a981622db7f427
azufb <curiousazunas@gmail.com>
2022-04-17 16:24:14 +0900

    ファイル名修正に伴うimportの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Form from "./components/form";
-import ToDos from './components/todos';
-import DoneToDos from "./components/doneTodos";
-import Achievement from "./components/achievement";
-import DeleteAllBtn from "./components/deleteAllBtn";
-import { Header } from "./components/header";
-import { Footer } from "./components/footer";
+import { Form } from './components/Form';
+import { ToDos } from './components/Todos';
+import { DoneToDos } from "./components/DoneToDos";
+import { Achievement } from "./components/Achievement";
+import { DeleteAllBtn } from "./components/DeleteAllBtn";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 export const App = () => {
     return (
