d0f08ccf4943626402bd4c7acfca443edf3e93d0
azufb <curiousazunas@gmail.com>
2022-04-17 23:27:15 +0900

    globalThemeの適用

--- src/styles/header.css.ts
+++ src/styles/header.css.ts
@@ -1,6 +1,7 @@
 import { style } from "@vanilla-extract/css";
+import { globalTheme } from './globalTheme.css';
 
 export const headerStyle = style({
-    backgroundColor: '#1A1A1A',
-    color: '#FFFFFF'
+    backgroundColor: globalTheme.background.main,
+    color: globalTheme.font.color
 });
