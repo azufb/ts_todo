6090ae39ee049b303620370fb01d2b3c1245d5be
azufb <curiousazunas@gmail.com>
2022-04-17 16:05:57 +0900

    アロー関数に変更

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -2,13 +2,12 @@ import React, { useEffect } from 'react';
 import { useForm, SubmitHandler } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { addToDo, calculateAchievement } from "../slice/slice";
-import { vanillaApp } from "../styles/styles.css";
 
 interface inputForm {
     task: String
 }
 
-function Form() {
+export const Form = () => {
     const dispatch = useDispatch();
     // isSubmitSuccessfulは、フォームの送信に成功したか否かを判定するboolean値。
     const { register, handleSubmit, reset, formState: { isSubmitSuccessful } } = useForm<inputForm>();
@@ -25,11 +24,9 @@ function Form() {
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <label className={vanillaApp}>Task：</label>
+            <label>Task：</label>
             <input { ...register("task")} />
             <input type="submit" />
         </form>
     );
 }
-
-export default Form;
