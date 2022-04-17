a3f4e2b496be2d874301fb7c567866c3bc9656e8
azufb <curiousazunas@gmail.com>
2022-04-17 23:27:20 +0900

    globalThemeの適用

--- src/styles/footer.css.ts
+++ src/styles/footer.css.ts
@@ -1,7 +1,8 @@
 import { style } from "@vanilla-extract/css";
+import { globalTheme } from './globalTheme.css';
 
 export const footerStyle = style({
     marginTop: 'auto',
-    backgroundColor: '#1A1A1A',
-    color: '#FFFFFF'
+    backgroundColor: globalTheme.background.main,
+    color: globalTheme.font.color
 })
