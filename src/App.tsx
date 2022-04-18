a9997772fc76f023f7b897837dcc51a5868fcda5
azufb <curiousazunas@gmail.com>
2022-04-18 21:29:40 +0900

    Sprinkles適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Form } from './components/Form';
-import { ToDos } from './components/Todos';
+import { Form } from './components/form';
+import { ToDos } from './components/ToDos';
 import { DoneToDos } from "./components/DoneToDos";
-import { Achievement } from "./components/Achievement";
+//import { Achievement } from "./components/Achievement";
 import { DeleteAllBtn } from "./components/DeleteAllBtn";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { sprinkles } from './styles/sprinkles.css';
 
 export const App = () => {
     return (
@@ -12,9 +13,11 @@ export const App = () => {
             <Header />
             <Form />
             <DeleteAllBtn />
-            <ToDos />
-            <DoneToDos />
-            <Achievement />
+            <div className={sprinkles({display: 'flex', justifyContent: 'center'})}>
+                <ToDos />
+                <DoneToDos />
+            </div>
+            {/*<Achievement />*/}
             <Footer />
         </>
     )
