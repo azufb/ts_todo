07e17986aeae83a34b5cccaff4ca2349bd111272
azufb <curiousazunas@gmail.com>
2022-04-20 00:08:01 +0900

    フォームのcss作成

--- src/styles/form.css.ts
+++ src/styles/form.css.ts
@@ -1,5 +1,8 @@
 import { style } from "@vanilla-extract/css";
 
-export const form = style({
-    
+export const formStyle = style({
+    textAlign: 'center',
+    paddingTop: '2rem',
+    paddingLeft: '2rem',
+    paddingRight: '2rem'
 })
