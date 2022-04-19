bdd670198977ca08c5f8cfd3196a24cba3c6edf4
azufb <curiousazunas@gmail.com>
2022-04-20 00:08:45 +0900

    左右の余白調整

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Form } from './components/Form';
 import { ToDos } from './components/ToDos';
 import { DoneToDos } from "./components/DoneToDos";
 //import { Achievement } from "./components/Achievement";
-import { DeleteAllBtn } from "./components/DeleteAllBtn";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { sprinkles } from './styles/sprinkles.css';
@@ -11,11 +10,12 @@ export const App = () => {
     return (
         <>
             <Header />
-            <Form />
-            <DeleteAllBtn />
-            <div className={sprinkles({display: 'flex', justifyContent: 'center'})}>
-                <ToDos />
-                <DoneToDos />
+            <div className={sprinkles({paddingLeft: {desktop: 'm', mobile: 'xs'}, paddingRight: {desktop: 'm', mobile: 'xs'}})}>
+                <Form />
+                <div className={sprinkles({display: 'flex', justifyContent: 'center'})}>
+                    <ToDos />
+                    <DoneToDos />
+                </div>
             </div>
             {/*<Achievement />*/}
             <Footer />
