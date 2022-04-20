4f9063b11c9f08556fbd5c48eeab89531a96c440
azufb <curiousazunas@gmail.com>
2022-04-20 22:48:08 +0900

    スタイルの当て方修正

--- src/components/deleteAllBtn.tsx
+++ src/components/deleteAllBtn.tsx
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import { deleteAllToDo } from "../slice/slice";
 import { themeClass, alertBtn } from "../styles/scopedTheme.css";
-import { sprinkles } from "../styles/sprinkles.css";
+import { deleteAllBtnStyle } from "../styles/deleteAllBtn.css";
 
 export const DeleteAllBtn = () => {
     const dispatch = useDispatch();
@@ -11,7 +11,7 @@ export const DeleteAllBtn = () => {
     }
 
     return (
-        <div className={`${themeClass} ${sprinkles({textAlign: 'center'})}`}>
+        <div className={`${themeClass} ${deleteAllBtnStyle}`}>
             <button onClick={() =>handleDeleteAll()} className={alertBtn}>全て削除する</button>
         </div>
     )
