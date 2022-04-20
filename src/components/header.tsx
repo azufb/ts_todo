1fe0756eb1b451795a13ae9ccd2e2d745bf5c9a3
azufb <curiousazunas@gmail.com>
2022-04-20 23:06:58 +0900

    ヘッダーの位置調整

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,9 @@
-import { headerStyle } from "../styles/header.css"
+import { headerStyle } from "../styles/header.css";
+import { sprinkles } from '../styles/sprinkles.css';
 
 export const Header = () => {
     return (
-        <div className={headerStyle}>
+        <div className={`${headerStyle} ${sprinkles({paddingLeft: {desktop: 'm', mobile: 'xs'}})}`}>
             <h1>タスク管理</h1>
         </div>
     )
