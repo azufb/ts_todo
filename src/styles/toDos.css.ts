e56e1e6cee15a5cceaae108134a586ea17c8e749
azufb <curiousazunas@gmail.com>
2022-04-20 22:49:33 +0900

    完了ボタン・削除ボタンのスタイル作成

--- src/styles/toDos.css.ts
+++ src/styles/toDos.css.ts
@@ -2,5 +2,10 @@ import { style } from "@vanilla-extract/css";
 import { globalTheme } from "./globalTheme.css";
 
 export const doneBtnStyle = style({
-    backgroundColor: globalTheme.background.success
-})
+    backgroundColor: globalTheme.background.success,
+    marginLeft: globalTheme.size.xs
+});
+
+export const deleteBtn = style({
+    marginLeft: globalTheme.size.xs
+});
