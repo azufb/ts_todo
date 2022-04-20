e3bf67328fa2ff5ebd782526fe0c4b97cbf429f9
azufb <curiousazunas@gmail.com>
2022-04-20 13:11:11 +0900

    タスク一覧箇所用にcss作成

---
+++ src/styles/toDos.css.ts
@@ -0,0 +1,6 @@
+import { style } from "@vanilla-extract/css";
+import { globalTheme } from "./globalTheme.css";
+
+export const doneBtnStyle = style({
+    backgroundColor: globalTheme.background.success
+})
