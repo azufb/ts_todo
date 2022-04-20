c6b1e8c0b42c4d78ec6b9420ac8af4c1525588dd
azufb <curiousazunas@gmail.com>
2022-04-20 22:49:12 +0900

    送信ボタン用のスタイル作成

--- src/styles/form.css.ts
+++ src/styles/form.css.ts
@@ -1,8 +1,14 @@
 import { style } from "@vanilla-extract/css";
+import { globalTheme } from "./globalTheme.css";
 
 export const formStyle = style({
     textAlign: 'center',
     paddingTop: '2rem',
+    paddingBottom: '2rem',
     paddingLeft: '2rem',
     paddingRight: '2rem'
-})
+});
+
+export const submitBtn = style({
+    marginLeft: globalTheme.size.xs
+});
