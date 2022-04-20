e54ee13897007c560392cf3dabfe0a44786b25c8
azufb <curiousazunas@gmail.com>
2022-04-20 22:49:43 +0900

    スタイル適用

--- src/components/todos.tsx
+++ src/components/todos.tsx
@@ -1,9 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
 import { calculateAchievement, changeStatus, deleteToDo } from "../slice/slice";
 import { RootState } from '../store/store';
-import { DeleteAllBtn } from "./DeleteAllBtn";
 import { sprinkles } from "../styles/sprinkles.css";
-import { doneBtnStyle } from "../styles/toDos.css";
+import { doneBtnStyle, deleteBtn } from "../styles/toDos.css";
 
 export const ToDos = () => {
     const dispatch = useDispatch();
@@ -23,13 +22,12 @@ export const ToDos = () => {
     return (
         <div className={sprinkles({width: '50%'})}>
             <h1 className={sprinkles({textAlign: 'center'})}>タスク一覧</h1>
-            <DeleteAllBtn />
             <ul>
                 {todo.map((item) => {
                 return ( 
                     <li key={item.id}>{item.title}
                         <button onClick={() => handleChangeStatus(item.id)} className={doneBtnStyle}>完了する</button>
-                        <button onClick={() => handleDeleteToDo(item.id)}>削除</button>
+                        <button onClick={() => handleDeleteToDo(item.id)} className={deleteBtn}>削除</button>
                     </li>
                 )
                 })}
